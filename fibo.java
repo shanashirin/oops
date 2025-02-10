fc01ecfccb40043ac60d67235c115763e03fc85c
shanashirin <103477393+shanashirin@users.noreply.github.com>
2025-02-10 14:33:39 +0530

    Add files via upload

---
+++ fibo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class fibo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of terms: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        int first = 0, second = 1, next;
+        
+   
+        System.out.print("Fibonacci series: " + first + " " + second);
+
+
+        for (int i = 2; i < n; i++) {
+            next = first + second;
+            System.out.print(" " + next);  
+            first=second;
+            second = next;
+        }
+        System.out.println();  
+    }
+}
+
